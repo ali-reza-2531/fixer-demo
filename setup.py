4d64bf68214252125eaa9e5911853959537c0411
Alireza shams <alirezashams09@gmail.com>
2025-07-22 09:23:20 +0330

    Initial commit

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name='fixer-demo', version=0.1, description='Fixer service demo package',
+      url='https://github.com/ali-reza-2531/fixer-demo', author='Alireza', author_email='alirezashams09@gamil.com',
+      license='MIT', packages=['fixer'], zip_safe=False)
