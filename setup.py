f52e058f3f2fcff885cefec460b2991b57c18f77
Alireza shams <alirezashams09@gmail.com>
2025-07-22 09:46:11 +0330

    Bump again  to version 2.0

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 
-setup(name='fixer-demo', version=0.1, description='Fixer service demo package',
+setup(name='fixer-demo', version=0.2, description='Fixer service demo package',
       url='https://github.com/ali-reza-2531/fixer-demo', author='Alireza', author_email='alirezashams09@gamil.com',
       license='MIT', requires=['requests'], packages=['fixer'], zip_safe=False)
